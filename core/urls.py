5e4917970ce6090aa187bbf880556d35e0a5bcb5
vicky765 <vickytiwari765@gmail.com>
2021-10-16 16:34:01 +0530

    stylized

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from chat.views import index
 urlpatterns = [
+    path("", index, name="home"),
     path('admin/', admin.site.urls),
     path('chat/', include('chat.urls'))
 ]
---
+++ static/js/master.js
@@ -0,0 +1,88 @@
+chatRoomName = document.getElementById('room-name');
+if (chatRoomName) {
+    const roomName = JSON.parse(chatRoomName.textContent);
+    let user_username = JSON.parse(document.getElementById('user_username').textContent);
+    if (!user_username) {
+        user_username = "Anonymous User";
+
+    }
+
+
+
+    document.querySelector("#submit").onclick = function (e) {
+        const messageInputDOM = document.querySelector("#input");
+        const message = messageInputDOM.value;
+        if (message) {
+            const setUsername = document.querySelector("#setUsername");
+            console.log(setUsername.value)
+            if (!setUsername.value) {
+                alert("enter username");
+                setUsername.focus();
+            } else {
+                setUsername.setAttribute('disabled', 'true');
+            }
+            if (setUsername.value) {
+                user_username = setUsername.value;
+                chatSocket.send(JSON.stringify({
+                    'message': message,
+                    'username': user_username,
+                }));
+                messageInputDOM.value = '';
+                messageInputDOM.focus();
+            }
+
+        }
+
+    }
+
+    document.querySelector("#chat_form").onsubmit = function (e) {
+        e.preventDefault();
+        // document.querySelector("#submit").click();
+    }
+    const chatSocket = new WebSocket(
+        'ws://' +
+        window.location.host +
+        '/ws/chat/' +
+        roomName +
+        '/'
+    );
+    chatSocket.onmessage = function (e) {
+        const data = JSON.parse(e.data);
+        console.log(data);
+        // document.querySelector("#user-hello").innerHTML = (data.tester);
+
+        document.querySelector("#chat-text").innerHTML += "<span class='message'> <span class='username'>" + (data
+            .username + ":</span> " + data.message +
+            "</span>");
+    }
+
+
+    chatRoomURLInput = document.querySelector("#chat_room_url");
+    chatRoomURLInput.value = location.protocol + '//' + location.host + "/chat/" + roomName + "/";
+
+}
+
+let CHAT_ROOMS = Array();
+for (let index = 0; index < 10; index++) {
+    // let r = (Math.random() + 1).toString(36).substring(2);
+    r = "room_" + index;
+    CHAT_ROOMS.push(r);
+}
+joinARoomBtn = document.querySelectorAll(".join-a-room");
+joinARoomBtn.forEach(element => {
+    element.addEventListener('click', joinARandomRoom);
+});
+
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function joinARandomRoom(e) {
+    e.preventDefault();
+    randInt = getRandomInt(0, CHAT_ROOMS.length);
+    randomRoom = CHAT_ROOMS[randInt];
+    window.location.href = location.protocol + '//' + location.host + "/chat/" + randomRoom + "/";
+}
