b52cd3bd19a0a59b8160c8a0cf60a1078c98db29
vicky765 <vickytiwari765@gmail.com>
2021-10-16 17:48:45 +0530

    fixed asgi setting

--- core/asgi.py
+++ core/asgi.py
@@ -1,8 +1,7 @@
 import os
 import django
 from channels.routing import get_default_application
-from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 django.setup()
-application = get_asgi_application()
+application = get_default_application()
